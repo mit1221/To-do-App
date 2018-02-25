9f486be3554029d745a1052a7c7bbf8a09a6dd6a
Mit <mit_rupang@yahoo.ca>
2018-02-25 12:55:14 -0500

    Done application!

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,28 +10,29 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var item1 = Todo({item: 'walk dog'}).save(function(err){
-  if (err) throw err;
-  console.log('Item saved');
-});
 
-var data = [{item: 'get milk'}, {item: 'walk'}, {item: 'coding'}];
+//var data = [{item: 'get milk'}, {item: 'walk'}, {item: 'coding'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
   app.get('/todo', function(req, res){
-    res.render('todo', {todos: data});
+    Todo.find({}, function(err, data){
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    });
   });
 
   app.post('/todo', urlencodedParser, function(req, res){
-    data.push(req.body);
-    res.json(data);
+    var newTodo = Todo(req.body).save(function(err, data){
+      if (err) throw err;
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item', function(req, res){
-    data = data.filter(function(todo){
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+      if (err) throw err;
+      res.json(data);
     });
-    res.json(data);
   });
 };
